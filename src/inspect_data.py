466683ee8c3fa10c791b89537367a89add008bb8
maryambani <119344521+maryambani@users.noreply.github.com>
2025-11-03 22:19:02 -0500

    Adding csv data

---
+++ src/inspect_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from pathlib import Path
+
+data_dir = Path("data")
+
+files = [
+    "iedb_tcell_pos.csv",
+    "iedb_tcell_neg.csv",
+    "cedar_tcell_pos.csv",
+    "cedar_tcell_neg.csv"
+]
